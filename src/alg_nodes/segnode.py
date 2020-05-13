700a4e23eae2a818309725a0c805de84b14eced5
Cézar Lemos <cezarcbl@protonmail.com>
2020-05-13 20:03:37 -0300

    fix paths

---
+++ src/alg_nodes/segnode.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+from detectron2 import model_zoo
+import __fix
+import rospy
+from std_msgs.msg import Float32
+from sensor_msgs.msg import PointCloud2, Image
+import sensor_msgs.point_cloud2 as pc2
+import numpy as np
+import message_filters
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+import os
+import cv2
+from lib3.nn import Segmenter 
+
+class ObstacleNN:
+
+    def __init__(self):
+
+        self.predictor = Segmenter()
+
+        self.color_img = None
+        self.rect_img = None
+        self.cloud = None
+
+        self.d = None
+        self.crest = None
+
+        self.bridge = CvBridge()
+        self.pub_dist = rospy.Publisher('/edge_distance', Float32, queue_size=10)
+        self.pub_img = rospy.Publisher('/image_edge_highlighted', Image, queue_size=10)
+        
+        rospy.Subscriber('/stereo_camera/left/image_rect_color', Image, self.color_img_callback)
+        rospy.Subscriber('/stereo_camera/left/image_rect', Image, self.rect_img_callback)
+        # rospy.Subscriber('/bumblebee2/points2', PointCloud2, self.points2_callback)
+
+        
+    def color_img_callback(self, data):
+        self.color_img = self.bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
+
+    def rect_img_callback(self, data):
+        self.rect_img = self.bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
+
+    def publish(self):
+        
+        if self.color_img is None: return
+
+        im = self.color_img
+        dim = self.predictor.pred_and_draw(im)
+        cv2.imshow('Segmentation', dim)
+        cv2.waitKey(3)
+        # raw_img_draw = self.draw_crest()
+        # out_img_msg = self.bridge.cv2_to_imgmsg(raw_img_draw)
+        # self.pub_img.publish(out_img_msg)
+
+        # dmsg = Float32()
+        # dmsg.data = self.d
+        # self.pub_dist.publish(dmsg)
+
+    
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('DistanceCloud', anonymous=True)
+
+    obsnn = ObstacleNN()
+    
+    rate = rospy.Rate(30)
+    while not rospy.is_shutdown():
+        # rospy.loginfo('Message sent!')
+        obsnn.publish()
+        rate.sleep()
+
+if __name__ == '__main__':
+    listener()
---
+++ src/lib3/nn.py
@@ -0,0 +1,41 @@
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import numpy as np
+import cv2
+import random
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+import matplotlib.pyplot as plt
+
+class Segmenter:
+
+    def __init__(self):
+        cfg = get_cfg()
+        self.cfg = cfg
+        cfg.merge_from_file(model_zoo.get_config_file("COCO-PanopticSegmentation/panoptic_fpn_R_50_3x.yaml"))
+        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-PanopticSegmentation/panoptic_fpn_R_50_3x.yaml")
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+        self.predictor = DefaultPredictor(cfg)
+
+    def __call__(self, im):
+        panoptic_seg, segments_info = self.predictor(im)["panoptic_seg"]
+        return panoptic_seg, segments_info
+
+    def pred_and_draw(self, im, im_format='cv2'):
+        panoptic_seg, segments_info = self(im)
+        v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]), scale=1.2)
+        v = v.draw_panoptic_seg_predictions(panoptic_seg.to("cpu"), segments_info)
+        im = v.get_image()
+        if im_format == 'cv2':
+            return im
+        else:
+            return im[:, :, ::-1]
+

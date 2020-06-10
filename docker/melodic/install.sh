6edb041e527e35d6b44125e509baa212b7086322
Cézar Lemos <cezarcbl@protonmail.com>
2020-06-10 01:11:07 -0300

    remove docker files

--- docker/melodic/install.sh
+++ docker/melodic/install.sh
@@ -10,7 +10,6 @@ source ~/.bashrc
 
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/
-# catkin config -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so
 catkin config --init
 echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
 catkin build
